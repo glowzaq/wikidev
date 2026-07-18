faf96a6b3330935149befbdb6df0a3542664e872
Inioluwa Akintokun <inioluwaakintokun@gmail.com>
2026-07-18 11:40:07 +0100

    update eslint.config.mjs file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript";
+import { FlatCompat } from "@eslint/eslintrc";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const compat = new FlatCompat({
+  // import.meta.dirname ensures relative module paths are resolved correctly
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript"
+    ],
+    settings: {
+      next: {
+        // Points ESLint to the correct root folder
+        rootDir: ".",
+      },
+    },
+  }),
+];
 
 export default eslintConfig;
